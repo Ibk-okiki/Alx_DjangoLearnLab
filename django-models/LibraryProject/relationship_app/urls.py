fb575ada2cb58d90f9668c439b0c5cdffa6b2232
Ibk-okiki <ibukunoluwaadelokiki@gmail.com>
2025-07-20 23:13:35 +0100

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -17,4 +17,7 @@ urlpatterns = [
     path('admin-page/', views.admin_view, name='admin_view'),
     path('librarian-page/', views.librarian_view, name='librarian_view'),
     path('member-page/', views.member_view, name='member_view'),
+    path('books/add/', views.add_book, name='add-book'),
+    path('books/edit/<int:pk>/', views.edit_book, name='edit-book'),
+    path('books/delete/<int:pk>/', views.delete_book, name='delete-book'),
 ]
