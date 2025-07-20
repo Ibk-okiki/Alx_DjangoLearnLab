4208a3edad9f139c8077766a52d48964fad44ad8
Ibk-okiki <ibukunoluwaadelokiki@gmail.com>
2025-07-20 23:03:38 +0100

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -13,4 +13,8 @@ urlpatterns = [
 
     # Logout view using Django’s built-in class-based view
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+
+    path('admin-page/', views.admin_view, name='admin_view'),
+    path('librarian-page/', views.librarian_view, name='librarian_view'),
+    path('member-page/', views.member_view, name='member_view'),
 ]
