0caf66f74fd10654643f90cbedf890a3d907bd72
Ibk-okiki <ibukunoluwaadelokiki@gmail.com>
2025-08-03 22:47:54 +0100

    Update settings.py

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -1,3 +1,19 @@
 # Add this at the bottom or near the AUTH section
 AUTH_USER_MODEL = 'bookshelf.CustomUser'
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False  # ✅ Set to False in production
+
+# ✅ Security Headers
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+# ✅ Cookie Security
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
+# ✅ Recommended Additional (if using HTTPS and custom domains)
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
