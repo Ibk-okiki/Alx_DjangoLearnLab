332c1b3dbf2c02e8da2a6dfe8fa13bceb8c6ba8f
Ibk-okiki <ibukunoluwaadelokiki@gmail.com>
2025-08-03 23:18:25 +0100

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework import viewsets
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
