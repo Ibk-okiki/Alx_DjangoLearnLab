703038c1aa6df8df76a80177b14f2605352a33cd
Ibk-okiki <ibukunoluwaadelokiki@gmail.com>
2025-08-18 00:02:10 +0100

    Update urls.py

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from api.views import AuthorViewSet, BookViewSet
-
+from .views import BookListCreateView, BookRetrieveUpdateDestroyView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include(router.urls)),
+    path("books/", BookListCreateView.as_view(), name="book-list-create"),
+    path("books/<int:pk>/", BookRetrieveUpdateDestroyView.as_view(), name="book-detail"),
 ]
 
 
