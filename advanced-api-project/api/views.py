2ccfb6eb64cdbf1fad664c988fc9bcdaa66fd287
Ibk-okiki <ibukunoluwaadelokiki@gmail.com>
2025-08-17 23:53:05 +0100

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Author, Book
+from .serializers import AuthorSerializer, BookSerializer
 
-# Create your views here.
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all().prefetch_related("books")
+    serializer_class = AuthorSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.select_related("author").all()
+    serializer_class = BookSerializer
